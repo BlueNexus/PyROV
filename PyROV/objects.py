a621fac590a63949ba64212633b00bd1ba291408
BlueNexus <benreeve99@yahoo.co.uk>
2016-03-01 14:02:55 +0000

    Added power cells

--- PyROV/objects.py
+++ PyROV/objects.py
@@ -8,3 +8,30 @@ class Object:
         self.z = z
         self.x = x
         self.y = y
+
+class Cell(Object):
+    name = "Cell"
+    icon = "="
+    total_power = 1000
+    
+    def __init__(self, x, y, z):
+        self.z = z
+        self.x = x
+        self.y = y
+        self.power = self.total_power
+
+class CellBasic(Cell):
+    name = "PC-Lite Cell"
+
+class CellMedium(Cell):
+    name = "PC-2K Cell"
+    total_power = 2000
+
+class CellLarge(Cell):
+    name = "PC-4K Cell"
+    total_power = 4000
+
+class CellXL(Cell):
+    name = "PC-16K Battery"
+    total_power = 16000
+    
