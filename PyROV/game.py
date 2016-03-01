a4da96a1a1cf103e54dfcc2dfaec4e9a9c7d288c
BlueNexus <benreeve99@yahoo.co.uk>
2016-03-01 11:25:47 +0000

    Finished command handling

--- PyROV/game.py
+++ PyROV/game.py
@@ -54,25 +54,42 @@ def show_commands():
 
 def handle_input(inp):
     global commands_list
+    global world
     if inp in commands_list:
-        if inp = "Q":
-            player.move(5)
-        elif inp = "W":
-            player.move(1)
-        elif inp = "E":
-            player.move(6)
-        elif inp = "A":
-            player.move(4)
-        elif inp = "S":
-            player.move(3)
-        elif inp = "D":
-            player.move(2)
-        elif inp = "?":
+        if inp == "Q":
+            player.move(5, world)
+        elif inp == "W":
+            player.move(1, world)
+        elif inp == "E":
+            player.move(6, world)
+        elif inp == "A":
+            player.move(4, world)
+        elif inp == "S":
+            player.move(3, world)
+        elif inp == "D":
+            player.move(2, world)
+        elif inp == "?":
             show_commands()
-        elif inp = "I":
+        elif inp == "I":
             player.show_inventory()
-        elif "G": #Will be finished later
-            
+        elif imp == "G":
+            available = player.get_adjacent_items(world)
+            print("Adjacent items: ")
+            for item in available:
+                print(item.name + "\n")
+            chose = str(raw_input("Choose which item to grab: "))
+            valid = False
+            for item in available:
+                if chose == item.name:
+                    player.grab(item, world)
+                    valid = True
+                    break
+            if not valid:
+                print("Invalid item!")
+        elif imp == "F":
+            player.drop(world)
+    else:
+        print("Invalid command. Enter '?' for a list of commands.")
 
 # World generation
 make_world(WORLD_Z, WORLD_X, WORLD_Y)
