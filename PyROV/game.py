a5950c6e0cb02cd1a37f8a15cb4a52ac2be1584e
BlueNexus <benreeve99@yahoo.co.uk>
2016-03-08 14:17:29 +0000

    [WIP] Changing the world into an object
    Extremely buggy. I also discovered that X is Y and Y is X. So we're going to end up with zyx, rather than zxy or xyz

--- PyROV/game.py
+++ PyROV/game.py
@@ -3,109 +3,166 @@ import blocks
 import objects
 import random
 
-# CONFIG
-WORLD_Z = 20
-WORLD_X = 40
-WORLD_Y = 40
-WORLDGEN_ROCK_CHANCE = 15
-WORLDGEN_OBJECT_CHANCE = 1
-PLAYER_VIEW_X = 6
-PLAYER_VIEW_Y = 6
-# END CONFIG
+player = None
 
-commands_dict = {"Q": "Move up", "W": "Move forward", "E": "Move down",
+''' Everything is broken. Will fix soon
+Todo: 
+Move grabbing and dropping into world
+Fix x and y being the wrong way round
+Unbreak all the stuff
+'''
+
+
+class World:
+	
+    commands_dict = {"Q": "Move up", "W": "Move forward", "E": "Move down",
             "A": "Move left", "S": "Move back", "D": "Move right",
             "U": "Use item", "?": "Show commands", "I": "Show inventory",
             "G": "Grab", "F": "Drop"}
-commands_list = ["Q", "W", "E", "A", "S", "D", "U", "?", "I", "G", "F"]
-world = []
-player = None
+	commands_list = ["Q", "W", "E", "A", "S", "D", "U", "?", "I", "G", "F"]
+	WORLD_Z = 20
+	WORLD_X = 40
+	WORLD_Y = 40
+	WORLDGEN_ROCK_CHANCE = 15
+	WORLDGEN_OBJECT_CHANCE = 1
+	PLAYER_VIEW_X = 6
+	PLAYER_VIEW_Y = 6
+	world = []
 
-def make_world(z, x, y):
-    print("Generating world...")
-    working = []
-    for plane in range(z):
-        print("Generating Z-level " + str(plane) + " of " + str(z))
-        wZ = []
-        for row in range(x):
-            wX = []
-            for col in range(y):
-                rand = random.randint(0, 100)
-                if rand > WORLDGEN_ROCK_CHANCE:
-                    wY = blocks.Water(plane, row, col)
-                elif rand > WORLDGEN_OBJECT_CHANCE:
-                    wY = blocks.Rock(plane, row, col)
-                else:
-                    wY = objects.Object(plane, row, col)
-                wX.append(wY)
-            wZ.append(wX)
-        working.append(wZ)
-    global world
-    global player
-    world = working
-    print("Creating the player...")
-    start_z = z / 2
-    start_x = x / 2
-    start_y = y / 2
-    player = entity.ROV(start_z, start_x, start_y)
-    world[player.z][player.x][player.y] = player
-    print("Generation complete!")
+    def __init__(self):
+        self.make_world(self.WORLD_Z, self.WORLD_X, self.WORLD_Y)
 
-def print_world(cZ, cX, cY):
-    global world
-    working = world[cZ]
-    for row in range(cX - PLAYER_VIEW_X, cX + PLAYER_VIEW_X):
-        wX = working[row]
-        fX = []
-        for col in range(cY - PLAYER_VIEW_Y, cY + PLAYER_VIEW_Y):
-            fX.append(wX[col].icon)
-        fX_joined = ' '.join(fX)
-        print(fX_joined)
+    def make_world(self, z, x, y):
+        working = []
+        for plane in range(z):
+            wZ = []
+            for row in range(x):
+                wX = []
+                for col in range(y):
+                    rand = random.randint(0, 100)
+                    if rand > WORLDGEN_ROCK_CHANCE:
+                        wY = blocks.Water(plane, row, col)
+                    elif rand > WORLDGEN_OBJECT_CHANCE:
+                        wY = blocks.Rock(plane, row, col)
+                    else:
+                        wY = objects.Object(plane, row, col)
+                    wX.append(wY)
+                wZ.append(wX)
+            working.append(wZ)
+        global player
+        self.world = working
+        print("Creating the player...")
+        start_z = z / 2
+        start_x = x / 2
+        start_y = y / 2
+        player = entity.ROV(start_z, start_x, start_y)
+        self.world[player.z][player.x][player.y] = player
+        print("Generation complete!")
 
-def show_commands():
-    global commands_dict
-    print(commands_dict)
+	def get_view_extents(self, thing, x, y):
+		pass
+
+    def print_world(self, thing):
+    	cur_Z = thing.z
+    	cur_X = thing.x
+    	cur_Y = thing.y
+        working = self.world[cur_Z]
+        north, east, south, west = self.get_view_extents(thing, cur_X, cur_Y)
+        for row in range(cur_X - north, cur_X + south):
+            work_X = working[row]
+            final_X = []
+            for col in range(cur_Y - west, cur_Y + east):
+                final_X.append(work_X[col].icon)
+            final_X_joined = ' '.join(final_X)
+            print(fX_joined)
+
+	def can_move(self, thing):
+        moveable = []
+        if self.world[thing.z][thing.x][thing.y + 1].passable:
+            moveable.append(1)
+        if self.world[thing.z][thing.x + 1][thing.y].passable:
+            moveable.append(2)
+        if self.world[thing.z][thing.x][thing.y - 1].passable:
+            moveable.append(3)
+        if self.world[thing.z][thing.x - 1][thing.y].passable:
+            moveable.append(4)
+        if self.world[thing.z + 1][thing.x][thing.y].passable:
+            moveable.append(5)
+        if self.world[thing.z - 1][thing.x][thing.y].passable:
+            moveable.append(6)
+        return moveable
+
+	def replace_with_water(self, thing):
+		self.world[thing.z][thing.x][thing.y] = blocks.Water(thing.z, thing.x, thing.y)
+
+	def sync_thing(self, thing):
+		self.world[thing.z][thing.x][thing.y] = thing
+
+	def place_thing(self, thing, z, x, y):
+		self.world[z][x][y] = thing
+
+    def step(self, direc, thing):
+        moveable = self.can_move(matter)
+        if moveable and direc in moveable:
+        	self.replace_with_water(thing)
+            if direc == 1:
+                thing.y += 1
+            elif direc == 2:
+                thing.x += 1
+            elif direc == 3:
+                thing.y -= 1
+            elif direc == 4:
+                thing.x -= 1
+            elif direc == 5:
+                thing.z += 1
+            elif direc == 6:
+                thing.z -= 1
+            self.sync_zxy(thing)
+        else:
+            print("Thunk.")
+
+	def show_commands(self):
+    	print(self.commands_dict)
 
 
-def handle_input(inp):
-    global world
-    if inp in commands_list:
-        if inp == "Q":
-            player.move(5, world)
-        elif inp == "W":
-            player.move(4, world)
-        elif inp == "E":
-            player.move(6, world)
-        elif inp == "A":
-            player.move(3, world)
-        elif inp == "S":
-            player.move(2, world)
-        elif inp == "D":
-            player.move(1, world)
-        elif inp == "U":
-            player.use()
-        elif inp == "?":
-            show_commands()
-        elif inp == "I":
-            player.show_inventory()
-        elif inp == "G":
-            available = player.get_adjacent_items(world)
-            print("Adjacent items: ")
-            for item in available:
-                print(item.name)
-            chose = str(raw_input("Choose which item to grab > "))
-            valid = False
-            for item in available:
-                if chose == item.name:
-                    player.grab(item, world)
-                    valid = True
-                    break
-            if not valid:
-                print("Invalid item!")
-        elif inp == "F":
-            player.drop(world)
-    else:
-        print("Invalid command. Enter '?' for a list of commands.")
+	def handle_input(self, inp):
+    	if inp in self.commands_list:
+        	if inp == "Q":
+            	self.step(5, player)
+        	elif inp == "W":
+            	self.step(4, player)
+        	elif inp == "E":
+            	self.step(6, player)
+        	elif inp == "A":
+            	self.step(3, player)
+        	elif inp == "S":
+            	self.step(2, player)
+        	elif inp == "D":
+            	self.step(1, player)
+        	elif inp == "U":
+            	player.use()
+        	elif inp == "?":
+            	self.show_commands()
+        	elif inp == "I":
+            	player.show_inventory()
+        	elif inp == "G":
+            	available = self.get_adjacent(player)
+            	print("Adjacent items: ")
+            	for item in available:
+                	print(item.name)
+            	chose = str(raw_input("Choose which item to grab > "))
+            	valid = False
+            	for item in available:
+                	if chose == item.name:
+                    	player.grab(item, self)
+                    	valid = True
+                    	break
+            	if not valid:
+                	print("Invalid item!")
+        	elif inp == "F":
+            	player.drop(self)
+    	else:
+        	print("Invalid command. Enter '?' for a list of commands.")
 
 def options_get_value(choice):
     return int(raw_input("Enter a value for " + str(editing) + " > "))
@@ -118,9 +175,9 @@ while True:
     print("2. Options")
     choice = str(raw_input("Enter your choice (Start/Options) > ")).title()
     if choice == "Start":
-        make_world(WORLD_Z, WORLD_X, WORLD_Y)
+    	globe = World()
         while True:
-            print_world(player.z, player.x, player.y)
+            globe.print_world(player)
             print("Power remaining: " + str(player.cell.power))
             handle_input(str(raw_input(">> ")).title())
 
@@ -159,4 +216,3 @@ while True:
                 pass
         except:
             pass
-
