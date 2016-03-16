421f879352726df861cd461d223d604d54989d56
BlueNexus <benreeve99@yahoo.co.uk>
2016-03-16 15:00:02 +0000

    Moved entity functions into world

--- PyROV/entity.py
+++ PyROV/entity.py
@@ -1,83 +1,31 @@
 import blocks
 import objects
 import random
-import time
-
-# 1 = north
-# 2 = east
-# 3 = south
-# 4 = west
-# 5 = up
-# 6 = down
-
 
 class Entity:
     can_grab = False
     name = ""
+    inventory = []
     icon = "e"
     can_move = True
 
-    def __init__(self, z, x, y, world):
+    def __init__(self, z, y, x, world):
         self.z = z
-        self.x = x
         self.y = y
+        self.x = x
 
     def get_coords(self):
-        return self.z, self.x, self.y
-
-
-    def get_adjacent_items(self, world):
-        adjacent = []
-        adjacent.append(world[self.z][self.x][self.y + 1])
-        adjacent.append(world[self.z][self.x][self.y - 1])
-        adjacent.append(world[self.z][self.x][self.x + 1])
-        adjacent.append(world[self.z][self.x][self.x - 1])
-        adjacent.append(world[self.z][self.x + 1][self.y + 1])
-        adjacent.append(world[self.z][self.x - 1][self.y + 1])
-        adjacent.append(world[self.z][self.x + 1][self.y - 1])
-        adjacent.append(world[self.z][self.x - 1][self.y - 1])
-        for thing in adjacent:
-            if isinstance(thing, blocks.Block):
-                adjacent.remove(thing)
-            if thing == self:
-                adjacent.remove(thing)
-        return adjacent
-
+        return self.z, self.y, self.x
 
 class ROV(Entity):
     name = "ROV"
-    inventory = []
     icon = "@"
 
-    def __init__(self, z, x, y):
+    def __init__(self, z, y, x):
         self.z = z
-        self.x = x
         self.y = y
-        self.cell = objects.CellBasic(self.z, self.x, self.y)
-
-    def move(self, direc, world):
-        moveable = self.can_move(world)
-        if self.can_move:
-            if moveable and direc in moveable:
-                world[self.z][self.x][self.y] = blocks.Water(self.z, self.x, self.y)
-                if direc == 1:
-                    self.y += 1
-                elif direc == 2:
-                    self.x += 1
-                elif direc == 3:
-                    self.y -= 1
-                elif direc == 4:
-                    self.x -= 1
-                elif direc == 5:
-                    self.z += 1
-                elif direc == 6:
-                    self.z -= 1
-                world[self.z][self.x][self.y] = self
-            else:
-                print("Thunk.")
-            self.power_tick(10)
-        else:
-            print("You can't move!")
+        self.x = x
+        self.cell = objects.CellBasic(self.z, self.y, self.x)
 
     def shutdown(self):
         self.can_move = False
@@ -102,35 +50,3 @@ class ROV(Entity):
             if i.name == choice:
                 i.activate()
 
-    def grab(self, item, world):
-        if item.can_grab:
-            world[item.z][item.x][item.y] = blocks.Water(item.z, item.x, item.y)
-            item.z, item.x, item.y = self.get_coords()
-            self.inventory.append(item)
-            print("Picked up " + item.name)
-            self.power_tick(5)
-        else:
-            print("It's stuck!")
-    
-    def drop(self, world):
-        self.show_inventory()
-        item = str(raw_input("Choose an item to drop: "))
-        for i in self.inventory:
-            if i.name == item:
-                i.z, i.x, i.y = self.get_coords()
-                avail = self.can_move(world)
-                direc = random.randint(1, 5)
-                if direc == 1:
-                    i.y += 1
-                elif direc == 2:
-                    i.x += 1
-                elif direc == 3:
-                    i.y -= 1
-                elif direc == 4:
-                    i.x -= 1
-                world[item.z][item.x][item.y] = item
-                self.inventory.remove(item)
-                self.power_tick(5)
-                break
-        else:
-            print("You don't have a " + item + "!")
