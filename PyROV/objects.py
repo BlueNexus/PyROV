21268a8efc33c718091459b8686f2ea3815ad44d
BlueNexus <benreeve99@yahoo.co.uk>
2016-03-24 14:29:00 +0000

    Added weight to batteries

--- PyROV/objects.py
+++ PyROV/objects.py
@@ -4,6 +4,8 @@ class Object:
     can_grab = True
     passable = True
     useable = False
+    vol = 1
+    weight = 1
 
     def __init__(self, z, y, x):
         self.z = z
@@ -14,7 +16,6 @@ class Cell(Object):
     name = "Cell"
     icon = "="
     total_power = 1000
-    useable = True
     
     def __init__(self, z, y, x):
         self.z = z
@@ -22,18 +23,31 @@ class Cell(Object):
         self.x = x
         self.power = self.total_power
 
+    def activate(self, user):
+        user.inventory.append(user.cell)
+        user.cell = self
+        user.inventory.remove(self)
+
 class CellBasic(Cell):
     name = "PC-Lite Cell"
+    vol = 1
+    weight = 2
 
 class CellMedium(Cell):
     name = "PC-2K Cell"
     total_power = 2000
+    vol = 4
+    weight = 8
 
 class CellLarge(Cell):
     name = "PC-4K Cell"
     total_power = 4000
+    vol = 8
+    weight = 16
 
 class CellXL(Cell):
     name = "PB-16K Battery"
     total_power = 16000
+    vol = 30
+    weight = 50
     
