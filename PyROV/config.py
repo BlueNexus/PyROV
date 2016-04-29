6146e6ca92c288b1db706de821c3a2defd9c3f05
BlueNexus <benreeve99@yahoo.co.uk>
2016-04-29 09:20:59 +0100

    Create config.py

---
+++ PyROV/config.py
@@ -0,0 +1,23 @@
+# Number of Z-levels in the world. Numerical value.
+WORLD_Z = 10
+
+# Number of Y-levels in the world, going both up and down, so if 20 was set, the world would be generated from -20 to 20. Numerical value.
+WORLD_Y = 20
+
+# Number of Y-levels in the world, going both up and down, so if 20 was set, the world would be generated from -20 to 20. Numerical value.
+WORLD_X = 20
+
+# Percentage chance of a rock tile being generated during worldgen. Value between 0 and 100.
+WORLD_ROCK_CHANCE = 15
+
+# Percentage chance of an object being generated during worldgen. Low values recommended, including decimals.
+WORLD_OBJECT_CHANCE = 0.05
+
+# Number of tiles the player can see both north and south.
+PLAYER_VIEW_Y = 6
+
+# Number of tiles the player can see both east and west.
+PLAYER_VIEW_X = 6
+
+# Enables debug features and expanded logging.
+DEBUG = True
