197002e5dc3245d52037ba2c8355bb485a5ecfed
BlueNexus <benreeve99@yahoo.co.uk>
2016-07-17 17:18:03 +0100

    New C:DDA-esque keyed inventory

--- PyROV/entity.py
+++ PyROV/entity.py
@@ -2,7 +2,9 @@ import blocks
 import objects
 import random
 import time
+import string
 
+charset = list(string.printable)
 # 1 = north
 # 2 = east
 # 3 = south
@@ -71,12 +73,26 @@ class ROV(Entity):
 
     def show_inventory(self):
         print("Items in inventory: ")
-        for i in self.inventory:
-            print(i + " " + i.name)
+        dict_inventory = self.key_list(self.inventory)
+        for key, value in dict_inventory.items():
+            print(key + ": " + value.s_name())
+
+    def get_keyed_inventory(self):
+        print("Items in inventory: ")
+        dict_inventory = self.key_list(self.inventory)
+        for key, value in dict_inventory.items():
+            print(key + ": " + value.s_name())
+        return(dict_inventory)
+
+    def key_list(self, old_list):
+        dictified = {}
+        for index, item in enumerate(old_list):
+            dictified[charset[index]] = item
+        return dictified
 
     def use(self):
-        self.show_inventory()
-        choice = str(raw_input("Choose an item to use: "))
-        for i in self.inventory:
-            if i.name == choice:
-                i.activate(self)
+        inv = self.get_keyed_inventory()
+        choice = str(input("Choose an item to use: "))
+        for key, value in inv.items():
+            if key == choice:
+                value.activate(self)
